d5aecf654b80d8b8ac5b1a41345e9da3898a82f6
Itish Srivastava <itish.srivastava@think41.com>
2025-04-02 14:59:10 +0530

    fixed the calendar event

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"C8jqmbNL3xQBtGKT6tdFCyPCjItNfjSLtATI4HN0CvI=\"\n}"
